21e9d6d6c31a17824e5aa069c96d7c63724f2327
Flor Farías <florencia.fariasmm@gmail.com>
2022-02-11 10:47:20 -0300

    Flor - migraciones

---
+++ src/database/migrations/20220211134539-create-product.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      stock_min: {
+        type: Sequelize.INTEGER
+      },
+      stock_max: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      extended_description: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      section: {
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      colorId: {
+        type: Sequelize.INTEGER
+      },
+      brandId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ src/database/models/product.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    stock: DataTypes.INTEGER,
+    stock_min: DataTypes.INTEGER,
+    stock_max: DataTypes.INTEGER,
+    description: DataTypes.STRING,
+    extended_description: DataTypes.STRING,
+    image: DataTypes.STRING,
+    section: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    colorId: DataTypes.INTEGER,
+    brandId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
